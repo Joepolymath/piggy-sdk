57a48344629c3b778a6a45ea31ef6f53332c7bee
Joepolymath <joshuaajagbe96@gmail.com>
2023-04-17 03:26:29 +0100

    rollup config updated

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+const banner = `// Piggy-SDK v${lib.version} Copyright (c) 2023 Joshua Ajagbe and contributors`;
 export default {
   input: 'src/index.js',
   output: [
@@ -5,13 +6,16 @@ export default {
       file: 'dist/index.js',
       format: 'cjs',
       sourcemap: true,
-      exports: 'named',
+      exports: 'default',
+      preferConst: true,
+      banner,
     },
     {
       file: 'dist/index.es.js',
       format: 'es',
       sourcemap: true,
       exports: 'named',
+      banner,
     },
   ],
 };
